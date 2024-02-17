086deefa3ee0cea58d76bfd9001edf0c8ed9b7ba
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-02-17 18:25:38 +0100

    refactor: Remove unnecessary routes in index.js

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ db.once('open', function() {
 app.use(express.json());
 app.use(cors()); // Use the cors middleware
 app.use('/api/v1/users', users);
-app.get('/', users);
-app.delete('/:id', users);
-app.get('/:id', users);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
