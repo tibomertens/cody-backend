18f7863e534087f45a8f427b8e45031251928c31
TiboMertens <tibomertens25@gmail.com>
2024-02-17 14:12:29 +0100

    feat: Add Express server setup and MongoDB connection

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+require('dotenv').config()
+
+const cors = require('cors'); // Import the cors middleware
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_PW);
+
+//check if the connection is established
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Connection Successful!");
+});
+
+//json body parser
+app.use(express.json());
+app.use(cors()); // Use the cors middleware
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
