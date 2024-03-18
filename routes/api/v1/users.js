d737151371406dfa0c8f9b16e839cc9ee61a127e
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2024-03-18 11:39:30 +0100

    feat: add users patch functionality

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -11,5 +11,6 @@ router.get("/", createUser.getUsers);
 router.delete("/:id", createUser.deleteUser);
 router.get("/:id", createUser.getUserById);
 router.post("/login", createUser.login);
+router.patch("/:id", createUser.updateUser);
 
 module.exports = router;
